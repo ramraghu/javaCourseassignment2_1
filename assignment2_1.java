75d50c7d2d6f86afd70865cda6176d5ceab143f7
ramraghu <raghuramnair2015@gmail.com>
2016-09-26 03:32:46 +0530

    First commit

---
+++ assignment2_1.java
@@ -0,0 +1,11 @@
+package com.acadgild.learnJava;
+
+public class assignment2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Print name without using semicolon
+	if (System.out.printf("Raghuram") == null) {
+		}
+	}
+}
